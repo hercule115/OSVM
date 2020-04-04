802e908a31fe8d9f8f119ac6ac4d3b4d62fa2ebc
didier <dspoirot@gmail.com>
2020-04-04 10:36:14 +0200

    Major rewrite of the video viewer, now using vlcMediaListPlayer

--- MediaViewerDialog.py
+++ MediaViewerDialog.py
@@ -246,11 +246,11 @@ class MediaViewerDialog(wx.Dialog):
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.videoOnTimer, self.timer)
 
-        # VLC player controls
-        self.vlcInstance = vlc.Instance()
-        self.mediaIsFinished = False
-
+        # GUI controls
         self._videoInitialize()
+
+        # VLC player controls
+        self._videoVlcInitialize()
 
     def _videoSetWindow(self, player):
         # set the window id where to render VLC's video output
@@ -261,6 +261,7 @@ class MediaViewerDialog(wx.Dialog):
             player.set_hwnd(handle)
         elif sys.platform == "darwin": # for MacOS
             player.set_nsobject(handle)
+            pass
 
     def _videoInitialize(self):
         self.panel1 = wx.Panel(id=wx.ID_ANY, name='panel1', parent=self, size=wx.DefaultSize, style=wx.TAB_TRAVERSAL)
@@ -280,13 +281,18 @@ class MediaViewerDialog(wx.Dialog):
 
         self.timeElapsed = wx.StaticText(self, label='00:00:00')
 
-        self.btnRew = wx.Button(self, label='Rew')
-        self.Bind(wx.EVT_BUTTON, self.videoOnBtnRew, self.btnRew)
+        self.btnPrev = wx.Button(self, label='Prev')
+        self.Bind(wx.EVT_BUTTON, self.videoOnBtnPrev, self.btnPrev)
 
         self.btnPlay = wx.Button(self, label='Play')
-        #        self.Bind(wx.EVT_BUTTON, self.videoOnBtnPlay, self.btnPlay)
-        self.btnPlay.Bind(wx.EVT_BUTTON, lambda evt: self.videoOnBtnPlay(evt))
+        self.Bind(wx.EVT_BUTTON, self.videoOnBtnPlayPause, self.btnPlay)
 
+        self.btnStop = wx.Button(self, label='Stop')
+        self.Bind(wx.EVT_BUTTON, self.videoOnBtnStop, self.btnStop)
+
+        self.btnNext = wx.Button(self, label='Next')
+        self.Bind(wx.EVT_BUTTON, self.videoOnBtnNext, self.btnNext)
+        
         self.btnVolume = wx.Button(self, label='Mute')
         self.Bind(wx.EVT_BUTTON, self.videoOnBtnVolume, self.btnVolume)
 
@@ -304,8 +310,10 @@ class MediaViewerDialog(wx.Dialog):
         self.timeSliderSizer.Add(self.timeSlider, 4)
         self.timeSliderSizer.AddStretchSpacer(prop=1)
 
-        self.btnSizer.Add(self.btnRew, 0, border=5, flag=wx.EXPAND| wx.ALL)
+        self.btnSizer.Add(self.btnPrev, 0, border=5, flag=wx.EXPAND| wx.ALL)
         self.btnSizer.Add(self.btnPlay, 0, border=5, flag=wx.EXPAND| wx.ALL)
+        self.btnSizer.Add(self.btnStop, 0, border=5, flag=wx.EXPAND| wx.ALL)
+        self.btnSizer.Add(self.btnNext, 0, border=5, flag=wx.EXPAND| wx.ALL)
         self.btnSizer.AddStretchSpacer(prop=1)
         self.btnSizer.Add(self.btnVolume, 0, border=5, flag=wx.EXPAND| wx.ALL)
         self.btnSizer.Add(self.volSlider, flag=wx.TOP | wx.LEFT, border=5)
@@ -317,74 +325,134 @@ class MediaViewerDialog(wx.Dialog):
         self.mainSizer.Add(self.timeSliderSizer, flag=wx.EXPAND, border=0)
         self.mainSizer.Add(self.btnSizer, 1, flag=wx.EXPAND)
 
-    def videoOnBtnPlay(self, event):
+    def _videoVlcInitialize(self):
+        # Create a VLC Instance
+        self.vlcInstance = vlc.Instance()  # '--verbose 1'.split())
+        # Create a VLC MediaListPlayer instance
+        self.vlcMLPlayer = self.vlcInstance.media_list_player_new()
+        # Create a VLC Player instance and bind it to our MediaListPlayer
+        self.vlcPlayer = self.vlcInstance.media_player_new()
+        self.vlcMLPlayer.set_media_player(self.vlcPlayer)
+        # Create a VLC Media List Instance
+        self.mediaList = self.vlcInstance.media_list_new() #ml
+        for e in self.listToUse: # each entry is a list containing filename as first field
+            filePath = os.path.join(self.videoDirName, e[globs.F_NAME])
+            myprint('Adding %s' % filePath)
+            media = vlc.Media(filePath)
+            self.mediaList.add_media(media)
+        self.vlcMLPlayer.set_media_list(self.mediaList)
+        # Bind the Media Player to our bitmap
+        self._videoSetWindow(self.vlcPlayer)
+        
+        # Create VLC Player Event Manager instances
+        self.vlcMLPEvent0 = self.vlcMLPlayer.event_manager()
+        # self.vlcMLPEvent0.event_attach(vlc.EventType.MediaListEndReached, self._videoMediaListEndReached)
+
+        self.vlcMLPEvent0.event_attach(vlc.EventType.MediaListPlayerNextItemSet, self._videoMediaListPlayerNextItemSet) #self._videoMediaListPlayerCallback)
+
+        self.vlcPEventS = self.vlcPlayer.event_manager()
+        self.vlcPEventS.event_attach(vlc.EventType.MediaPlayerStopped, self._videoMediaPlayerStopped)
+
+        self.vlcPEventP = self.vlcPlayer.event_manager()
+        self.vlcPEventP.event_attach(vlc.EventType.MediaPlayerPaused, self._videoMediaPlayerPaused)
+        
+        self.vlcPEventE = self.vlcPlayer.event_manager()
+        self.vlcPEventE.event_attach(vlc.EventType.MediaPlayerEndReached, self._videoMediaPlayerFinished)
+        # Tweak configuration depending on current parameters
+        if self.mediaList.count() == 1:
+            self.btnNext.Disable()
+
+    def _videoReset(self):
+        if self.vlcMLPlayer.is_playing():
+            myprint('Stopping VLC MediaListPlayer')
+            self.vlcMLPlayer.stop()
+            time.sleep(1)
+        # Release VLC objects
+        self.vlcPlayer.release()
+        self.vlcMLPlayer.release()
+        self.vlcInstance.release()
+        myprint('VLC Instance released')
+
+    ## Events
+    def _videoMediaListPlayerNextItemSet(self, event):
+        # Set Window Title
+        mediaTitle = vlc.Media(event.u.media).get_meta(vlc.Meta.Title)
+        self.SetTitle(mediaTitle)
+        self.btnPlay.SetLabel('Pause')
+        
+    def videoOnBtnPrev(self, event):
+        """Rewind to previous/beginning of media."""
+        myprint('Rewinding Media')
+        if self.vlcMLPlayer.previous():
+            myprint('No previous item')
+        self.btnStop.Enable()
+        event.Skip()            
+        
+    def videoOnBtnPlayPause(self, event):
         button = event.GetEventObject()
 
-        for e in self.listToUse: # each entry is a list containing filename as first field
-            v = os.path.join(self.videoDirName, e[globs.F_NAME])
-            myprint('Playing %s' % v)
-            self.mediaIsFinished = False
+        if button.GetLabel() == 'Play':
+            button.SetLabel('Pause')
+            self._videoOnBtnPlay()
+        else:
+            button.SetLabel('Play')
+            self._videoOnBtnPause()
+        event.Skip()
 
-            # Create a VLC Player instance
-            self.vlcPlayer = self.vlcInstance.media_player_new()
-            self._videoSetWindow(self.vlcPlayer)
-            # Create a VLC Player Event Manager instance
-            self.vlcEvents = self.vlcPlayer.event_manager()
-            self.vlcEvents.event_attach(vlc.EventType.MediaPlayerEndReached, self._videoMediaFinished)
-            self.vlcEvents.event_attach(vlc.EventType.MediaPlayerStopped, self._videoPlayerStopped)
-
-            # Create a VLC Media instance
-            media = self.vlcInstance.media_new(v)
-            self.vlcPlayer.set_media(media)
-
-            title = self.vlcPlayer.get_title()
-            # if an error was encountred while retrieving the title, 
-            # then use the filename
-            if title == -1:
-                title = os.path.basename(v)
-            self.SetTitle(title)
-
-            # Try to launch the media, if this fails display an error message
-            if self.vlcPlayer.play() == -1:
-                dlg = wx.MessageDialog(self, 'Unable to play', 'Error', wx.OK|wx.ICON_ERROR)
-                dlg.ShowModal()
-            else:
-                self.playerIsStopped = False
-                self.timer.Start(globs.TIMER2_FREQ)
-                while not self.mediaIsFinished:
-                    wx.Yield() 
-
-        print('videoOnBtnPlay(): End of list')
-        self.EndModal(wx.ID_OK)
-#        event.Skip()
-
-    def videoOnBtnRew(self, event):
-        """Stop the player."""
-        self.vlcPlayer.stop()
+    def _videoOnBtnPause(self):
+        myprint('Pausing Player')
+        if self.vlcMLPlayer.is_playing():
+            self.vlcMLPlayer.pause()
+            
+    def _videoOnBtnPlay(self):            
+        """ Try to launch the media. If it fails, display an error message."""
+        if self.vlcMLPlayer.play() == -1:
+            dlg = wx.MessageDialog(self, 'Unable to play', 'Error', wx.OK|wx.ICON_ERROR)
+            dlg.ShowModal()
+            self._videoReset()    # Destroy VLC objects
+            self.Close()
+            return
+        myprint('MediaListPlayer is playing...')
+        self.timer.Start(globs.TIMER2_FREQ)
+        self.btnStop.Enable()
+        
+    def videoOnBtnStop(self, event):
+        """Stop the Player."""
+        myprint('Stopping Player')
+        self.vlcMLPlayer.stop()
+        self.btnPlay.SetLabel('Play')        
         # reset the time slider
         self.timeSlider.SetValue(0)
-        self.timer.Stop()
+        self.timeElapsed.SetLabel('00:00:00')
+        self.SetTitle('Media Viewer')
+        self.btnStop.Disable()
+        event.Skip()        
 
+    def videoOnBtnNext(self, event):
+        """Skip to next item."""
+        myprint('Skipping to next item in list')        
+        if self.vlcMLPlayer.next():
+            myprint('No next item')
+        self.btnStop.Enable()
+        event.Skip()
+        
     def videoOnTimer(self, event):
         # since the self.player.get_length can change while playing,
         # re-set the timeslider to the correct range.
         self.timeSlider.SetRange(-1, self.vlcPlayer.get_length())
 
-        if self.playerIsStopped:
-            self.timer.Stop()
+        #if self.playerIsStopped:
+        if not self.vlcMLPlayer.is_playing():            
             return
-
-        # update the time on the slider
-        if not self.mediaIsFinished:
+        else:
+            # update the time on the slider
             self.elapsed = self.vlcPlayer.get_time()
             sec =  self.elapsed / 1000
             m, s = divmod(sec, 60)
             h, m = divmod(m, 60)
-            print('%02d:%02d\r' % (m,s), end='', flush=True)
+            #print('%02d:%02d\r' % (m,s), end='', flush=True)
             self.timeSlider.SetValue(self.elapsed)
             self.timeElapsed.SetLabel('%02d:%02d:%02d' % (h,m,s))
-        else:
-            self.timer.Stop()
         event.Skip()
 
     def videoOnBtnVolume(self, event):
@@ -397,17 +465,18 @@ class MediaViewerDialog(wx.Dialog):
         if button.GetName() == 'btnMute':
             button.SetName('btnVolume') 
             button.SetLabel('Volume')
-            self.oVolume = self.vlcPlayer.audio_get_volume()
+            self.oVolume = self.vlcMLPlayer.audio_get_volume()
             self.oVolSlider = self.volSlider.GetValue()
-            self.vlcPlayer.audio_set_volume(0)
+            self.vlcMLPlayer.audio_set_volume(0)
             self.volSlider.SetValue(0)
-            print('0:',self.oVolume,self.oVolSlider)
+            myprint('Muting:', 'ovolume=',self.oVolume,'pos=',self.oVolSlider)
         else:
             button.SetName('btnMute') 
             button.SetLabel('Mute')
-            self.vlcPlayer.audio_set_volume(self.oVolume)
+            self.vlcMLPlayer.audio_set_volume(self.oVolume)
             self.volSlider.SetValue(self.oVolSlider)
-            print('1:',self.oVolume,self.oVolSlider)
+            myprint('Unmuting:', 'ovolume=',self.oVolume,'pos=',self.oVolSlider)
+            myprint('1:',self.oVolume,self.oVolSlider)
 
     def videoOnTimeSlider(self, event):
         """Set the time according to the time sider."""
@@ -420,7 +489,7 @@ class MediaViewerDialog(wx.Dialog):
     def videoOnVolSlider(self, event):
         """Set the volume according to the volume sider."""
         volume = self.volSlider.GetValue() * 2
-        if self.vlcPlayer.audio_set_volume(volume) == -1:
+        if self.vlcMLPlayer.audio_set_volume(volume) == -1:
             dlg = wx.MessageDialog(self, 'Failed to set volume', 'Error', wx.OK|wx.ICON_ERROR)
             dlg.ShowModal()
         if volume > 0:
@@ -428,26 +497,24 @@ class MediaViewerDialog(wx.Dialog):
             self.btnVolume.SetLabel('Mute')
 
     def videoOnBtnQuit(self, event):
-        if self.vlcPlayer.is_playing():
-            print('Stopping VLC')
-            self.vlcPlayer.stop()
-            self.vlcPlayer.release()
-            self.vlcInstance.release()
-            print('VLC Released')
-            self.mediaIsFinished = True
-        else:
-            self._videoPlayerStopped(True)
-
-    def _videoPlayerStopped(self, evt):
+        self._videoReset()
+        # Stop the timer
+        self.timer.Stop()
+        self.Close()
+        event.Skip()
+        
+    # def _videoMediaListEndReached(self, event):
+    #     myprint('End of media list reached')
+    
+    def _videoMediaPlayerStopped(self, event):
         myprint('Player stopped')
-        self.playerIsStopped = True
 
-    def _videoMediaFinished(self, evt):
-        myprint('End of Media reached')
-        time.sleep(2)
-        self.mediaIsFinished = True
-        self.vlcPlayer.release()
+    def _videoMediaPlayerPaused(self, event):
+        myprint('Player paused')
 
+    def _videoMediaPlayerFinished(self, event):
+        myprint('Media Player finished')
+        self.btnPlay.SetLabel('Play')
 
 ########################
 def myprint(*args, **kwargs):
@@ -462,13 +529,24 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, id, title):
         wx.Frame.__init__(self, parent, id, title)
         panel = wx.Panel(self)
-#        filePath = os.path.join( os.getcwd(), 'images', 'plus-32.jpg')
-        filePath = '/Users/didier/SynologyDrive/Photo/TEST2/n.jpg'
-        dlg = MediaViewerDialog(self, filePath)
+        #filePath = os.path.join( os.getcwd(), 'images', 'plus-32.jpg')
+        #filePath = '/Users/didier/SynologyDrive/Photo/TEST2/n.jpg'
+        #filePath = '/Users/didier/SynologyDrive/Photo/Olympus TG4/P3302742.MOV'
+
+        mediaList = list()
+        mediaList.append(['/Users/didier/SynologyDrive/Photo/Olympus TG4/P3302741.MOV'])
+        mediaList.append(['/Users/didier/SynologyDrive/Photo/Olympus TG4/P3302742.MOV'])
+        mediaList.append(['/Users/didier/SynologyDrive/Photo/Olympus TG4/P3302739.MOV'])
+
+        filePath = '/Users/didier/SynologyDrive/Photo/Olympus TG4/P3302742.MOV'
+        
+        dlg = MediaViewerDialog(self, mediaList)
+        #dlg = MediaViewerDialog(self, filePath)
         ret = dlg.ShowModal()
         dlg.Destroy()
 
         self.Show()
+        self.Destroy()
 
 def main():
     # Init Globals instance
@@ -477,11 +555,16 @@ def main():
         globs.disabledModules.append(('VLC',msg))
 
     # Create a list of image files containing a single file
-#    globs.localFileInfos['plus-32.jpg'] = ['plus-32.jpg', 0, 0, '']
-    globs.localFileInfos['n.jpg'] = ['n.jpg', 0, 0, '']    
+    #globs.localFileInfos['plus-32.jpg'] = ['plus-32.jpg', 0, 0, '']
+    #globs.localFileInfos['n.jpg'] = ['n.jpg', 0, 0, '']
+
+    globs.localFileInfos['P3302741.MOV'] = ['P3302741.MOV', 0, 0, '']
+    globs.localFileInfos['P3302742.MOV'] = ['P3302742.MOV', 0, 0, '']
+    globs.localFileInfos['P3302739.MOV'] = ['P3302739.MOV', 0, 0, '']
+
     globs.localFilesSorted = sorted(list(globs.localFileInfos.items()), key=lambda x: int(x[1][globs.F_DATE]), reverse=globs.fileSortRecentFirst)
     
-            # Create DemoFrame frame, passing globals instance as parameter
+    # Create DemoFrame frame, passing globals instance as parameter
     app = wx.App(False)
     frame = MyFrame(None, -1, title="Test")
     frame.Show()
